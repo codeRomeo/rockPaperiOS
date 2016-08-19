af192cb14fcb8a4d771a69bd4404c09230958ece
codeRomeo <Tariq@tariqs-macbook-pro.local>
2016-08-19 18:15:21 -0400

    Initial commit of Rock Paper Scissors game ported from OS X

--- rockPaperiOS/ViewController.swift
+++ rockPaperiOS/ViewController.swift
@@ -9,15 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    let modelInstance = appModel()   //  initialize instance of Model object
+    
+    // various properites of ViewController (Interface builder)
+    
+    @IBOutlet weak var siriChoice: UILabel!
+    @IBOutlet weak var meChoice: UILabel!
+    @IBOutlet weak var resultsDisplay: UILabel!
+    @IBOutlet weak var meScore: UILabel!
+    @IBOutlet weak var siriScore: UILabel!
+    
+    
+ 
+    
+     @IBAction func rockPressed(_ sender: AnyObject) {
+        updateUI(choice: "rock")    // call this method to refresh View after game play
+     }
+    
+    @IBAction func paperPressed(_ sender: AnyObject) {
+        
+        updateUI(choice: "paper")   // call this method to refresh View after game play
     }
-
+    
+    @IBAction func ScissorsPressed(_ sender: AnyObject) {
+        
+        updateUI(choice: "scissors")   // call this method to refresh View after game play
+    }
+    
+    
+    override func viewDidLoad() {   // (Boilerplate code by Xcode)
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        resultsDisplay.text = "Pick your choice to play"   // set initial message and initialize view when app is loaded
+        
+    }
+    
+  func updateUI(choice:String ) {
+        
+        //  This method is called when user selects a choice. user choice string is updated with selection and calls the appModel class instance.
+        //  modelInstance() return game play result and scores. view is updated
+        
+        modelInstance.userChoice = choice                                 //  set user choice from user input
+        resultsDisplay.text  = modelInstance.play()              //  update result field with game play result (game play is trigerred once)
+ 
+        siriChoice.text = modelInstance.computerChoice           //  update user choice text field
+        meChoice.text = modelInstance.userChoice                 //  update computer choice text field
+        siriScore.text = "\(modelInstance.computerScore)"        //  update computer score
+        meScore.text = "\(modelInstance.userScore)"              //  update user score
+        
+        
+    }
+ 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
---
+++ rockPaperiOS/appModel.swift
@@ -0,0 +1,130 @@
+//
+//  appModel.swift
+//  rockPaperiOS
+//
+//  Created by Tariq on 8/15/16.
+//  Copyright © 2016 Tariq. All rights reserved.
+//
+
+import Foundation
+
+// ROCK * PAPER * SCISSORS
+//
+// Game play is self explanatory. In this MacOS implementation, user is presented with a window to choose rock, paper or scissors and plays against the computer.
+// Score is kept for both players. User and compouter choices are displayed as left and right boxes; score displays below each.
+// User picks their choice through three push buttons
+//
+// This is code version 0.2.0
+// Comments added for every class, function and variable
+//
+
+class appModel {
+    
+    //     This class is the model for "game logic" in the MVC pattern.
+    //     In theory, this source code can be ported to any platform without modification because its independent from either View or Controller.
+    //
+    //     properties :
+    //
+    //        appModel.userChoice is the input choice made by user between Rock, Paper or Scissors
+    //        appModel.computerChoice is the choice of Computer (gameBOT) randomized between Rock, Paper or Scissors
+    //        appModel.uiMessage - reserved for future updates
+    //        appModel.userScore - Score keeping for user
+    //        appModel.computerScore - Score keeping for computer
+    //
+    //     Methods:
+    //
+    //        appModel.play() - method (a function) that starts play on user input, determines winner and return winner message
+    //        appModel.compare - sub method and main game logic; compares two inputs and returns a winner according to traditional rules of Rock, Paper Scissors
+    //        appModel.randomizeRPS() - sub method; generates a random choice between Rock, Paper or Scissors for computer choice.
+    
+    // * properties *
+    
+    var userChoice = ""         //  String variable to hold input choice made by user between Rock, Paper or Scissors
+    var computerChoice = ""     //  String variable to hold the choice of Computer (gameBOT) randomized between Rock, Paper or Scissors
+    var uiMessage = ""          //  reserved for future updates
+    var userScore = 0           //  Int variable to hold user Score
+    var computerScore = 0       //  Int variable to hold computer score
+    var outcome:Int = 3
+    
+    
+    
+    
+    func play() -> String {
+        
+        var result = ""  // initialize a result string variable
+        
+        computerChoice = randomizeRPS()                             // randomize a choice string for computer
+        
+        result = compare(userChoice, compChoice: computerChoice)    // ask for user input and compare; store in result variable ; update scores
+        
+        switch result {
+            
+        case "User":        userScore+=1; outcome = 1
+            
+        case "Computer":    computerScore+=1; outcome = 2
+            
+        case "None": outcome = 3; break
+            
+        default: outcome = 3; break
+            
+        }
+        
+        print(computerScore, userScore)   // output to console scores (for testing; this line will not be in production
+        
+        return uiMessage     // return winner
+    }
+    
+    // * method *
+    
+    func compare (_ usrChoice: String, compChoice: String) -> String {
+        
+        //  This method (function) is the meat of the game logic. It takes two choices (strings) as arguments and compares to each other to determine the winner.
+        
+        var winner: String
+        
+        switch usrChoice {
+            
+        case compChoice:  winner = "None" ;  uiMessage = "Oh Gee, It's a tie! 😐"    // quick test to eliminate a tie scenario
+            
+            
+        case "rock":        if (compChoice == "scissors" )  { winner = "User"; uiMessage = "Rock breaks Scissors 😂"  }   else    {   winner = "Computer"; uiMessage = "Paper covers rock ☹️"   }  // check two scenarios
+            
+        case "paper":       if (compChoice == "rock" )  { winner = "User"; uiMessage = "Paper wraps rock 😀"  }   else    {   winner = "Computer"; uiMessage = "Scissors cuts paper 😰" } // check two scenarios
+            
+        case "scissors":    if (compChoice == "rock" )  { winner = "Computer"; uiMessage = "Rock dunces Scissors 😣"  }   else    {   winner = "User"; uiMessage = "Scissors rips paper 😎" } // check two scenarios
+            
+        default:            winner = "None" ; uiMessage = "It's a tie! 😐"
+            
+        }
+        
+        return winner // return outcome as string
+        
+    }
+    
+    
+    // * method *
+    
+    func randomizeRPS() -> String {
+        
+        //  This method (function) returns a random choice between Rock, Paper or Scissors as String.
+        //  Pls. note that arc4random_uniform() function is a Darwin specific call in module #Foundation; may not be present in Linux etc.
+        
+        let randomNum: Int = Int(arc4random_uniform(3)+1) // function call to return a random integer number between 1 to 3
+        
+        switch randomNum {   // return a selection based on random number
+            
+        case 1: return "rock"
+            
+        case 2: return "paper"
+            
+        case 3: return "scissors"
+            
+        default : return ""
+            
+        }
+        
+    }
+    
+    
+}
+
